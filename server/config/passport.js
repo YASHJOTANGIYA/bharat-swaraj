4bcbe41286c904a7566179fd3ddf600ec60b8fca
YASHJOTANGIYA <yashjotangiya0@gmail.com>
2025-12-09 10:25:12 +0530

    Fix duplicate proxy config in passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -13,7 +13,8 @@ if (process.env.GOOGLE_CLIENT_ID &&
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: '/api/auth/google/callback'
+                callbackURL: '/api/auth/google/callback',
+                proxy: true
             },
             async (accessToken, refreshToken, profile, done) => {
                 try {
